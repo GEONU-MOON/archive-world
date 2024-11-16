5e7407e624b49b9ecb6b032197c09efd7cfa76ff
GEONU-MOON <moondy2209@naver.com>
2024-11-16 17:13:44 +0900

    #15 refact : 로그인, 프로필 사진 라우트 MySQL연동

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -1,36 +1,45 @@
 const express = require("express");
 const router = express.Router();
 const { imageUploader, uploadToS3 } = require("../imageUploader");
-const { findUser } = require("../util/util");
+const connectDB = require("../db");
+const jwt = require("jsonwebtoken");
 
-
+// 프로필 사진 업로드
 router.post("/upload-profile", imageUploader.single("image"), (req, res, next) => uploadToS3(req, res, next, "profile"), async (req, res) => {
   try {
     const imageURL = req.imageUrl;
-    const currentUser = await findUser(req.headers.authorization);
-    currentUser.user_avatar = imageURL;
-    await currentUser.save();
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.userId;
+
+    const connection = await connectDB();
+    await connection.query("UPDATE Users SET user_avatar = ? WHERE user_id = ?", [imageURL, userId]);
+    await connection.end();
+
     res.status(200).json({ message: "Profile picture uploaded successfully", imageURL });
   } catch (error) {
     res.status(500).json({ error: "Failed to upload profile picture" });
   }
 });
 
+// 사용자 정보 조회
 router.get("/get-user-info", async (req, res) => {
   try {
-    // findUser 함수를 사용하여 현재 사용자의 정보를 가져옴.
-    const currentUser = await findUser(req.headers.authorization);
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.userId;
 
-    if (currentUser) {
-      res.status(200).json({
-        user_id: currentUser.user_id,
-        user_avatar: currentUser.user_avatar,
-      });
+    const connection = await connectDB();
+    const [userRows] = await connection.query("SELECT user_id, user_avatar FROM Users WHERE user_id = ?", [userId]);
+    const user = userRows[0];
+    await connection.end();
+
+    if (user) {
+      res.status(200).json(user);
     } else {
       res.status(404).json({ error: "User not found" });
     }
   } catch (error) {
-    // console.error("Error fetching user info:", error);
     res.status(500).json({ error: "Failed to fetch user info" });
   }
 });
